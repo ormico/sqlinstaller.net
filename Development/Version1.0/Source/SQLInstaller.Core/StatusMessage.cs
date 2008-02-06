f71af2a4f0ded41776f22cebb57e17f04b46366a
SND\bschloz_cp <SND\bschloz_cp@localhost>
2008-02-06 20:26:01 +0000

    Initial raw checkin of console app and supporting assembly.

---
+++ Development/Version1.0/Source/SQLInstaller.Core/StatusMessage.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLInstaller.Core
+{
+	public enum StatusMessage
+	{
+		Start,
+		Detail,
+		Progress,
+		Running,
+		Complete,
+		Exit,
+	}
+}
